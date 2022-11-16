33fa40f45acf76a40ce61e50090940e8331eeda3
Florian Calabrese florian.calabrese@gmail.com <florian.calabrese@gmail.com>
2022-11-16 17:08:52 +0100

    ok ex 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,15 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "0770357959", category: "chinese" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0770357959", category: "chinese" }
+paul_bosue = { name: "Paul Bocuse", address: "Rue de collonges", phone_number: "0770357959", category: "chinese" }
+
+[dishoom, pizza_east, paul_bosue].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
